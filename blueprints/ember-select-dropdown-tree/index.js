8561f78f790c75e66963f924ffe4fa8b61e0a1ca
Bj <btecu@outlook.com>
2016-11-03 12:38:38 -0400

    Update dependency

--- blueprints/ember-select-dropdown-tree/index.js
+++ blueprints/ember-select-dropdown-tree/index.js
@@ -6,7 +6,7 @@ module.exports = {
 
   afterInstall: function(options) {
     return this.addPackagesToProject([
-      { name: 'ember-select', target: '0.0.8' },
+      { name: 'ember-select', target: '^0.1.3' },
       { name: 'ember-simple-tree', target: '0.0.1' }
     ]);
   }
